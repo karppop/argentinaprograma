6a0bdb398a5e6d62c8fa69e3d0d2aa1e2af3c104
karppop <gcarballo111@gmail.com>
2023-06-10 15:44:49 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,12 @@
+let base = 5;
+let altura = 3;
+let areaRectangulo = base * altura;
+console.log(areaRectangulo);
+
+base = 12.5;
+altura = 5.3;
+areaRectangulo = 12.5 * 5.3;
+console.log(areaRectangulo);
+
+
+
---
+++ promedio3.js
@@ -0,0 +1,15 @@
+ x = 5;
+ y = 7;
+ z = 9;
+let promedio = (x + y + z) / 3;
+console.log(promedio);
+x = -2;
+y = -3;
+z = -8;
+promedio = (x + y + z) / 3;
+console.log(promedio);
+x = -1.2;
+y = 5.3;
+z = -3.3;
+promedio = (x + y + z) / 3;
+console.log(promedio);
