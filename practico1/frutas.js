8738a68acbf2298177efc60aa39c87e04563119f
karppop <gcarballo111@gmail.com>
2023-06-12 22:23:55 -0300

    Practico1

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas = ["Palta", "Mandarina", "Naranja", "Melon","Pera"];
+let ultima = frutas[4];
+console.log("La ultima fruta agregada fue: "+ultima);
+let primera = frutas[0];
+console.log("La primera fruta agregada fue: "+primera);
